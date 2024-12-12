c638facfab8a35109738a43b72e89f2cad50cbda
Rafayel Tobelyan <tobelyan@gmail.com>
2024-12-12 23:11:14 +0400

    Refactor send method to use async/await and improved error handling

--- index.js
+++ index.js
@@ -10,30 +10,35 @@ class ElasticEmailTransport {
         this.apiUrl = options.apiUrl || 'https://api.elasticemail.com/v2/email/send';
     }
 
-    send(mail, callback) {
-        const emailData = {
-            apikey: this.apiKey,
-            subject: mail.data.subject,
-            from: mail.data.from,
-            to: mail.data.to,
-            bodyHtml: mail.data.html || undefined,
-            bodyText: mail.data.text || undefined,
-            isTransactional: mail.data.isTransactional !== undefined ? mail.data.isTransactional : true,
-        };
+    async send(mail, callback) {
+        try {
+            const emailData = {
+                apikey: this.apiKey,
+                subject: mail.data.subject,
+                from: mail.data.from,
+                to: mail.data.to,
+                bodyHtml: mail.data.html || undefined,
+                bodyText: mail.data.text || undefined,
+                isTransactional: mail.data.isTransactional !== undefined ? mail.data.isTransactional : true,
+            };
 
-        axios
-            .post(this.apiUrl, null, { params: emailData })
-            .then((response) => {
-                callback(null, {
-                    accepted: [mail.data.to],
-                    rejected: [],
-                    response: response.data,
-                });
-            })
-            .catch((error) => {
-                callback(error);
+            const response = await axios.post(
+                this.apiUrl,
+                new URLSearchParams(emailData).toString(),
+                { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+            );
+
+            callback(null, {
+                accepted: [mail.data.to],
+                rejected: [],
+                response: response.data,
             });
+        } catch (error) {
+            console.error('Error sending email:', error.response ? error.response.data : error.message);
+            callback(error);
+        }
     }
+
 }
 
 module.exports = (options) => {
